0abf7da074d84048422d3ef51c2bf90bacabb14e
Chandimal <harischandrax@gmail.com>
2018-01-29 12:14:26 +0530

    add read me file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # python
+
+Commands to run project 
+
+1 - Check python version 
+
+python --version
+
+2 - Start Server 
+
+python manage.py runserver
+
+url to home page - http://localhost:8000/stories/
+
+
+
+
+
+
--- stories/views.py
+++ stories/views.py
@@ -6,7 +6,7 @@ from django.db.models import Count
 
 class IndexView(generic.ListView):
     template_name = 'stories/index.html'
-    context_object_name = 'all_stories';
+    context_object_name = 'all_stories'
 
     def get_queryset(self):
         return Story.objects.all().order_by('-story_created')[:5]
@@ -20,12 +20,12 @@ class CreateStory(CreateView):
     fields = ['story_title', 'story_content']
 
 class RatingView(generic.ListView):
-    template_name = 'stories/rating.html'
-    context_object_name = 'rating_count';
-
-    def get_queryset(self):
-        id = self.request.POST.get('story_id', False)
-        return Rating.objects.filter(story_id=id).values('rating_value').annotate(total=Count('rating_value'))
-
     model = Rating
     fields = ['rating_value', 'story']
+    template_name = 'stories/rating.html'
+    context_object_name = 'rating_count'
+
+    def get_queryset(self):
+        #id = request.POST.get('story_id', "")
+        return Rating.objects.all().values('rating_value').annotate(total=Count('rating_value'))
+
